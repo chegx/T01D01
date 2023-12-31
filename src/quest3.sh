aded747bc393a42701c747534040860f715d60fb
chegx <chegsar@gmail.com>
2023-12-31 21:37:21 +0900

    add mv, chmod commands

--- src/quest3.sh
+++ src/quest3.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
 mv door_management_fi door_management_files
+cd door_management_files
+# /src/door_management_files$
+mkdir door_configuration door_logs door_map
+mv *.conf door_configuration
+mv *.log door_logs
+mv door_map_1.1 door_map
+cd ..
+# /src$
+chmod +x ai_door_control.sh
